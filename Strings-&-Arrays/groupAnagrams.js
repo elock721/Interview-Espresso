e8a218883c62d3204756654b2e6684ed4080b28a
Elliott Lockwood <lockwood721@gmail.com>
2020-09-26 22:41:42 -0500

    group anagram notes

--- Strings-&-Arrays/groupAnagrams.js
+++ Strings-&-Arrays/groupAnagrams.js
@@ -1,17 +1,35 @@
+// Group Anagrams: Leet Code #49
+
+// Problem: Given an array of strings, return the strings grouped by anagram
+
+// anagram: when two words have the same letters, but they can be in a different order
+
+// INPUT: array of strings[]
+// OUTPUT: array of array of strings string[][]
+
+// EDGE CASE: getting passed an empty array as an input
+// ASSUMPTIONS: checking for complete anagrams, partial matches don't count
+
+// General Notes: you can sort anagrams with sort() function, more practice with hash table
+
+// PSEUDO CODE: 
+// loop through the string input list =>
+// sort each string =>
+// sorted key in table? add to array =>
+// sorted key not in table? create array
+//
+//
+//
+//
+//
+
+
+
+
+
 var groupAnagrams = function(string) {
     const ht = {};
 }
 
 
-var a = {}
-
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-
-for(let i = 0; i < array.length; i++){
-    if (condition) {
-        
-    }
-}
+// 
