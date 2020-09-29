6b7933368cefa2113c4323e29c046b6f30376b7e
Elliott Lockwood <lockwood721@gmail.com>
2020-09-29 08:36:01 -0500

    valid parentheses file

---
+++ Strings-&-Arrays/validParentheses.js
@@ -0,0 +1,56 @@
+/* 
+
+Valid Parentheses: Leet Code #20
+
+Data Strucure: the stack
+
+Problem: Given a string s containing just the characters 
+'(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+INPUT:
+
+OUTPUT:
+
+EDGE CASES:
+
+ASSUMPTIONS:
+
+GENERAL NOTES:
+
+PSEUDO CODE:
+
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// BIG O ^ 
+
+/* 
+
+Time Complexity - 
+
+-------------------------------------------
+
+Space Complexity - 
+
+
+*/
