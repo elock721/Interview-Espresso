7eccbd8ea344cfb9368239720d1e1e2b473fc371
Elliott Lockwood <lockwood721@gmail.com>
2020-10-06 22:36:20 -0500

    rotate image function complete

--- Strings-&-Arrays/rotateImage.js
+++ Strings-&-Arrays/rotateImage.js
@@ -21,7 +21,9 @@ EDGE CASES: na
 ASSUMPTIONS: matrix will always hold numbers
 
 GENERAL NOTES: draw out examples for matrix's, 
-reverse array and flip the coordinates
+reverse array and flip the coordinates,
+in JS the reverse method will work on any array (mutating the array not making a copy like splice),
+SLICE METHOD MAKES A COPY OF THE ARRAY
 
 PSEUDO CODE:
 
@@ -32,8 +34,17 @@ for each row ->
 
 */ 
 
-
-
+// 
+var rotate = function(matrix) {
+  matrix.reverse();  
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+        let temp = matrix [i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp; 
+    }
+  }
+};
 
 
 
