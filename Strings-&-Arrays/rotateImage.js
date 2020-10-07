55716064a616374efcfbea430da96f1ff7ac31f7
Elliott Lockwood <lockwood721@gmail.com>
2020-10-06 22:24:44 -0500

    matrix and rotate image notes

--- Strings-&-Arrays/rotateImage.js
+++ Strings-&-Arrays/rotateImage.js
@@ -4,19 +4,31 @@ Rotate Image: Leet Code #48
 
 Problem: 
 
+You are given an n x n 2D matrix representing an image
+
+Rotate the image by 90 degrees (clockwise)
+
+You have to rotate the image IN PLACE (no second matrix)
+
 ALGO/DS: Matrix - a 2 dimensional array
 
-INPUT:
+INPUT: Matrix nxn [][]number
 
-OUTPUT:
+OUTPUT: nothing 
 
-EDGE CASES:
+EDGE CASES: na
 
-ASSUMPTIONS:
+ASSUMPTIONS: matrix will always hold numbers
 
-GENERAL NOTES:
+GENERAL NOTES: draw out examples for matrix's, 
+reverse array and flip the coordinates
 
 PSEUDO CODE:
+
+Reverse outer arrays
+for each row ->
+    * for each column
+        * flip coordinates 
 
 */ 
 
---
+++ Strings-&-Arrays/zeeMatrixNotes.md
@@ -0,0 +1,6 @@
+Matrix Notes
+
+In a matrix problem you are generally traversing or transforming the matrix
+
+A matrix is just an array of arrays with each array representing a row in the matrix ->
+group columns by indices 
