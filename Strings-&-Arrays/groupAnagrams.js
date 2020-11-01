f7ba3ec062ca1c9060363bd67f077b5896407c19
Elliott Lockwood <lockwood721@gmail.com>
2020-10-31 23:22:23 -0500

    group anagrams extra method

--- Strings-&-Arrays/groupAnagrams.js
+++ Strings-&-Arrays/groupAnagrams.js
@@ -62,3 +62,15 @@ n = length of stings
 
 
 */
+
+
+for(let i=0;i<text.length;i++){
+    for(let j=text.length-1;j>i;j--){
+      let sortedA = text[i].split("").sort().join("")
+      let sortedB = text[j].split("").sort().join("")
+      if(sortedA===sortedB){
+        text.splice(j,1)
+      }
+    }
+  }
+  return text.sort()
